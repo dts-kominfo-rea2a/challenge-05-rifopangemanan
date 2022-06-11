0c04148ebda762d63157364a049ef0bd8a08c1c8
RifoPangemanan <rifopangemanan@gmail.com>
2022-06-11 16:44:53 +0700

    done

--- index.js
+++ index.js
@@ -6,15 +6,31 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (arr, cbfnSort) => {
+  sortData = cbfnSort(arr);
+  let arrhasil = [];
+
+  for (let counter = 0; counter < sortData.length; counter++) {
+    let loop = "";
+    loop += counter + 1 + ". " + sortData[counter];
+    arrhasil.push(loop);
+  }
+  return arrhasil;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (arr) => {
+  let data = arr.sort();
+  return data;
+};
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+const sortDescending = (arr) => {
+  let data = arr.sort().reverse();
+  return data;
+};
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
